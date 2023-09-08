31ab22c42b7e116e5a18925af420dccfb8b25e80
luigitarallo <tarallo.gigi@gmail.com>
2023-09-08 18:32:26 +0200

    feat: add data to appHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,64 @@
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      links: [
+        {
+          text: "CHARACTERS",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "COMICS",
+          url: "#",
+          state: true,
+        },
+        {
+          text: "MOVIES",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "TV",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "GAMES",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "COLLECTIBLES",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "VIDEOS",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "FANS",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "NEWS",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "SHOP",
+          url: "#",
+          state: false,
+        },
+      ],
+    };
+  },
+};
 </script>
-<template></template>
+<template>
+  <header></header>
+</template>
 <style></style>
