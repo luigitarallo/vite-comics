26096bb748ba9ed2e4b215c2279180ef57bb36e0
luigitarallo <tarallo.gigi@gmail.com>
2023-09-09 16:43:36 +0200

    feat: add li debug elements

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -59,6 +59,35 @@ export default {
 };
 </script>
 <template>
-  <header></header>
+  <header>
+    <div class="debug">Logo</div>
+    <div class="debug">
+      <ul>
+        <li class="debug">ciao</li>
+        <li class="debug">ciao</li>
+        <li class="debug">ciao</li>
+        <li class="debug">ciao</li>
+        <li class="debug">ciao</li>
+        <li class="debug">ciao</li>
+        <li class="debug">ciao</li>
+        <li class="debug">ciao</li>
+        <li class="debug">ciao</li>
+        <li class="debug">ciao</li>
+      </ul>
+    </div>
+  </header>
 </template>
-<style></style>
+<style lang="scss" scoped>
+@use "../styles/partials/mixins" as *;
+header {
+  @include flexed-on-line;
+  height: 100%;
+  ul {
+    list-style-type: none;
+    display: flex;
+    li {
+      padding: 0 1rem;
+    }
+  }
+}
+</style>
