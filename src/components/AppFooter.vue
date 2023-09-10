429076bcdc07adac083b379d7429211b76655804
luigitarallo <tarallo.gigi@gmail.com>
2023-09-10 11:24:20 +0200

    style: footer-sign-up, button

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -26,7 +26,23 @@ export default {
         </div>
       </div>
     </div>
-    <div></div>
+    <div class="footer-social debug">
+      <div class="container">
+        <div class="footer-sign-up">
+          <button>SIGN-UP NOW!</button>
+        </div>
+        <div class="footer-social-links">
+          <ul>
+            <li><span class="social-claim">FOLLOW US</span></li>
+            <li><img src="/img/footer-facebook.png" alt="" /></li>
+            <li><img src="/img/footer-twitter.png" alt="" /></li>
+            <li><img src="/img/footer-youtube.png" alt="" /></li>
+            <li><img src="/img/footer-pinterest.png" alt="" /></li>
+            <li><img src="/img/footer-periscope.png" alt="" /></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 <style lang="scss" scoped>
@@ -50,6 +66,36 @@ export default {
 
     img {
       width: 450px;
+    }
+  }
+}
+.footer-social {
+  background-color: #303030;
+  .footer-sign-up {
+    margin: 1.5rem 0;
+    padding: 0.8rem;
+    border: 2px solid #0c7cec;
+
+    button {
+      border-style: none;
+      background-color: #303030;
+      color: white;
+      cursor: pointer;
+    }
+  }
+  .footer-social-links {
+    color: white;
+    ul {
+      display: flex;
+      list-style-type: none;
+      align-items: center;
+      li {
+        margin-left: 1rem;
+      }
+      .social-claim {
+        color: #0c7cec;
+        font-weight: 600;
+      }
     }
   }
 }
