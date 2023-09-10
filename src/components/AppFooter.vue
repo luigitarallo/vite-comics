39d5c8251de48cd7d8706553e722add8e6cd6671
luigitarallo <tarallo.gigi@gmail.com>
2023-09-10 10:49:11 +0200

    feat: add footer big logo

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,56 @@
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  },
+};
 </script>
-<template></template>
-<style></style>
+<template>
+  <footer>
+    <div></div>
+    <div class="footer-site-links">
+      <div class="container debug">
+        <div class="site-links-list debug">
+          <ul>
+            li*3
+          </ul>
+          <ul>
+            li*3
+          </ul>
+          <ul>
+            li*3
+          </ul>
+        </div>
+        <div class="logo-big">
+          <img src="/img/dc-logo-bg.png" alt="big logo" />
+        </div>
+      </div>
+    </div>
+    <div></div>
+  </footer>
+</template>
+<style lang="scss" scoped>
+@use "../styles/general.scss" as *;
+
+.footer-site-links {
+  background-image: url(img/footer-bg.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 300px;
+
+  .footer-site-links {
+    display: flex;
+  }
+  .logo-big {
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+
+    img {
+      width: 450px;
+    }
+  }
+}
+</style>
