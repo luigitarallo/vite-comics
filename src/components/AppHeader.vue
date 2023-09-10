0be54b924aa0720e74f971e9d74a2028f4de729f
luigitarallo <tarallo.gigi@gmail.com>
2023-09-10 10:49:52 +0200

    feat: add logo and nav links

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -60,34 +60,48 @@ export default {
 </script>
 <template>
   <header>
-    <div class="debug">Logo</div>
-    <div class="debug">
-      <ul>
-        <li class="debug">ciao</li>
-        <li class="debug">ciao</li>
-        <li class="debug">ciao</li>
-        <li class="debug">ciao</li>
-        <li class="debug">ciao</li>
-        <li class="debug">ciao</li>
-        <li class="debug">ciao</li>
-        <li class="debug">ciao</li>
-        <li class="debug">ciao</li>
-        <li class="debug">ciao</li>
-      </ul>
+    <div class="container">
+      <div class="logo-small">
+        <img src="img/dc-logo.png" alt="logo small" />
+      </div>
+      <div>
+        <ul>
+          <li v-for="link in links" :class="link.state ? 'link-selected' : ''">
+            <a :href="link.url">{{ link.text }}</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </header>
 </template>
 <style lang="scss" scoped>
-@use "../styles/partials/mixins" as *;
+@use "../styles/general.scss" as *;
 header {
-  @include flexed-on-line;
-  height: 100%;
-  ul {
-    list-style-type: none;
-    display: flex;
-    li {
-      padding: 0 1rem;
-    }
+  background-color: white;
+}
+.logo-small {
+  img {
+    width: 5.5rem;
   }
+}
+ul {
+  display: flex;
+  list-style: none;
+  font-weight: 600;
+  font-size: 0.8rem;
+
+  li {
+    padding: 3rem 0;
+    margin-left: 2rem;
+  }
+}
+
+.link-selected {
+  color: #0c7cec;
+  border-bottom: 5px solid #0c7cec;
+}
+a {
+  text-decoration: none;
+  color: inherit;
 }
 </style>
