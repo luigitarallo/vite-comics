bcc79e008a8a321fe6e8a5dbadab5dcf061786bc
luigitarallo <tarallo.gigi@gmail.com>
2023-09-10 17:35:03 +0200

    feat: add appmain.vue

---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+export default {};
+</script>
+<template>
+  <main>
+    <div class="container debug">
+      <p>--Contents goes here!--</p>
+    </div>
+  </main>
+</template>
+<style lang="scss" scoped>
+@use "../styles/general.scss" as *;
+main {
+  background-color: #1c1c1c;
+  height: 100px;
+  p {
+    color: white;
+  }
+}
+</style>
