b33f5d2bf7686b8372f89ae45007f6bdf87d0868
luigitarallo <tarallo.gigi@gmail.com>
2023-09-11 17:24:10 +0200

    feat: add style to card

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -5,9 +5,12 @@ export default {
 </script>
 <template>
   <section>
-    <div class="card-container debug">
+    <div class="card-container">
       <figure v-for="comic in comics" :key="comic.thumb">
-        <img :src="comic.thumb" alt="" />
+        <div class="card">
+          <img :src="comic.thumb" alt="" />
+        </div>
+
         <figcaption>{{ comic.series }}</figcaption>
       </figure>
     </div>
@@ -21,19 +24,34 @@ section {
   background-color: $main-bg;
   display: flex;
   justify-content: center;
+  padding: 2rem 0;
+}
 
-  .card-container {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    width: 70%;
-  }
-  figure {
-    width: 25%;
-    gap: 2%;
-  }
+.card-container {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  gap: 0.5%;
+  width: 70%;
+}
+.card {
+  width: 160px;
+  height: 170px;
+  overflow: hidden;
+}
+
+figure {
+  width: 16%;
+  height: 16%;
+
   img {
     max-width: 100%;
+  }
+
+  figcaption {
+    color: $text-color;
+    font-size: 0.8rem;
+    padding: 1rem 0;
   }
 }
 </style>
