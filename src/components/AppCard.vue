443cad69b0ed4e0d22e2bee394cb00592775ff74
luigitarallo <tarallo.gigi@gmail.com>
2023-09-11 16:35:38 +0200

    feat: add v-for figure

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -3,5 +3,37 @@ export default {
   props: { comics: Object },
 };
 </script>
-<template></template>
-<style></style>
+<template>
+  <section>
+    <div class="card-container debug">
+      <figure v-for="comic in comics" :key="comic.thumb">
+        <img :src="comic.thumb" alt="" />
+        <figcaption>{{ comic.series }}</figcaption>
+      </figure>
+    </div>
+  </section>
+</template>
+<style lang="scss" scoped>
+@use "../styles/general.scss" as *;
+@use "../styles/partials/variables" as *;
+
+section {
+  background-color: $main-bg;
+  display: flex;
+  justify-content: center;
+
+  .card-container {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 70%;
+  }
+  figure {
+    width: 25%;
+    gap: 2%;
+  }
+  img {
+    max-width: 100%;
+  }
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,21 +3,19 @@ export default {};
 </script>
 <template>
   <main>
-    <div class="container">
-      <p>--Contents goes here!--</p>
-    </div>
+    <div class="container"></div>
   </main>
 </template>
 <style lang="scss" scoped>
 @use "../styles/general.scss" as *;
 @use "../styles/partials/variables" as *;
 main {
-  background-color: #1c1c1c;
+  background-image: url(../../public/img/jumbotron.jpg);
   p {
     color: $text-color;
   }
   .container {
-    height: 150px;
+    height: 400px;
   }
 }
 </style>
